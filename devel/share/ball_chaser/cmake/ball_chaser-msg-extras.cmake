36a187d6a2ae6b379f8b51b3464a68a7c2db285e
maurosterpin <msterpindev@gmail.com>
2022-11-27 13:14:40 +0000

    world update

--- devel/share/ball_chaser/cmake/ball_chaser-msg-extras.cmake
+++ devel/share/ball_chaser/cmake/ball_chaser-msg-extras.cmake
@@ -1,2 +1,2 @@
 set(ball_chaser_MESSAGE_FILES "")
-set(ball_chaser_SERVICE_FILES "/home/workspace/new_ws/src/ball_chaser/srv/DriveToTarget.srv")
+set(ball_chaser_SERVICE_FILES "/home/workspace2/Map-My-World/src/ball_chaser/srv/DriveToTarget.srv")
